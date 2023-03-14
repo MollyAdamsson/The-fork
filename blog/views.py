cd051c91f4626f44e3408ab1f9bb82e258b9eff6
Molly Adamsson <adamssonmolly@gmail.com>
2023-03-14 13:04:00 +0000

    Add changes to merge

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render, redirect
 from .forms import BookingForm
 
+
+def book_table(request):
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+
 # Create your views here.
