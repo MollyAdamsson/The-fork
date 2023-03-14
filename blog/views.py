5c522e76458343afa0dbcc39a93854b1795dd32e
Molly Adamsson <adamssonmolly@gmail.com>
2023-03-14 13:10:29 +0000

    Add booking system to view.py file and start of success message

--- blog/views.py
+++ blog/views.py
@@ -5,5 +5,13 @@ from .forms import BookingForm
 def book_table(request):
     if request.method == 'POST':
         form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('booking_success')
+        else:
+            form = BookingForm()
+    return render(request, 'book_table.html', {'form': form})
 
-# Create your views here.
+
+def booking_success(request):
+    return render(request, 'booking_sucess.html')
