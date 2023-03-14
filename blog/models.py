ff911fa5449f547599be9f909af80a5fda9dd0a0
Molly Adamsson <adamssonmolly@gmail.com>
2023-03-14 12:46:35 +0000

    Start with the booking form

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+
+
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    date = models.DateField()
+    time = models.TimeField()
+    num_guests = models.PositiveIntegerField()
